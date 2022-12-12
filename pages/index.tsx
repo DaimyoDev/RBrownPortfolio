c9821da4dbd1b5443c199d10015e492edbc2680a
Raymond Brown <daimyoplays@gmail.com>
2022-12-12 16:06:59 -0600

    header and background styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello!</h1>
+    <div className="text-center w-screen h-max bg-blue-800 animate-changebackground">
+      <div className="grid grid-cols-2 pt-[10rem] pb-[10rem] gap-[5rem] px-[5rem]">
+        <div>
+          <h1 className="text-9xl pt-[5rem] uppercase font-nunito text-white drop-shadow-[0_8px_3px_rgba(0,0,0,0.40)]">
+            Raymond Brown
+          </h1>
+          <h1 className="text-[3.7rem] pt-[2rem] bg-gradient-to-r from-[#ff4e02] to-[#f1e600] text-transparent bg-clip-text font-opensans pb-2 drop-shadow-[0_6px_3px_rgba(0,0,0,0.60)] font-semibold">
+            Front End Software Engineer
+          </h1>
+        </div>
+        <div className="bg-gradient-to-r from-[#605d5c] to-[#1f1d01] rounded-full h-[600px] w-[600px]">
+          <Image
+            className="rounded-full w-[600px] h-[600px] object-cover bg-clip-transparent opacity-80 drop-shadow-[0_8px_7px_rgba(0,0,0,0.60)]"
+            src={"/20221201_153151.jpg"}
+            alt="Raymond Brown Photo"
+            width={600}
+            height={600}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      keyframes: {
+        changebackground: {
+          "0%": {
+            "background-color": "rgb(30 64 175)",
+          },
+          "20%": {
+            "background-color": "rgb(55 48 163)",
+          },
+          "40%": {
+            "background-color": "rgb(6 95 70)",
+          },
+          "60%": {
+            "background-color": "rgb(30 64 175)",
+          },
+          "80%": {
+            "background-color": "rgb(153 27 27)",
+          },
+          "100%": {
+            "background-color": "rgb(159 18 57)",
+          },
+        },
+      },
+      animation: {
+        changebackground:
+          "changebackground 120s ease-in-out infinite alternate",
+      },
+      plugins: [],
+    },
   },
-  plugins: [],
 };
